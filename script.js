bb230a1e539ba98a389514b76d70062239a79ebb
punchyface <s10222101@connect.np.edu.sg>
2022-01-10 21:56:08 +0800

    added new attributes

--- script.js
+++ script.js
@@ -4,39 +4,44 @@
 $(document).ready(function () {
   //what kind of interface we want at the start 
   const APIKEY = "61d662beccd0211b32089770";
-  getContacts();
-  $("#update-contact-container").hide();
+  getStudents();
+  $("#update-student-container").hide();
   $("#add-update-msg").hide();
 
   //[STEP 1]: Create our submit form listener
-  $("#contact-submit").on("click", function (e) {
+  $("#student-submit").on("click", function (e) {
     //prevent default action of the button 
     e.preventDefault();
-    
-    tocheck("contact-name", "add-error");
-    tocheck("contact-email", "add-error");
-    tocheck("contact-msg", "add-error");
+    tocheck("student-id", "add-error");
+    tocheck("student-name", "add-error");
+    tocheck("student-email", "add-error");
+    tocheck("student-class", "add-error");
+    tocheck("mentorName", "add-error");
 
     //[STEP 2]: let's retrieve form data
     //for now we assume all information is valid
     //you are to do your own data validation
-    let contactName = $("#contact-name").val();
-    let contactEmail = $("#contact-email").val();
-    let contactMessage = $("#contact-msg").val();
+    let studentName = $("#student-name").val();
+    let studentEmail = $("#student-email").val();
+    let mentorName = $("#mentorName").val();
+    let studentClass = $("#student-class").val();
+    let studentId = $("#student-id").val();
 
     //[STEP 3]: get form values when user clicks on send
     //Adapted from restdb api
     let jsondata = {
-      "name": contactName,
-      "email": contactEmail,
-      "message": contactMessage
+      "ID": studentId,
+      "name": studentName,
+      "email": studentEmail,
+      "class": studentClass,
+      "mentorName": mentorName
     };
 
     //[STEP 4]: Create our AJAX settings. Take note of API key
     let settings = {
       "async": true,
       "crossDomain": true,
-      "url": "https://interactivedev-8bc3.restdb.io/rest/contact",
+      "url": "https://interactivedev-8bc3.restdb.io/rest/student",
       "method": "POST", //[cher] we will use post to send info
       "headers": {
         "content-type": "application/json",
@@ -49,12 +54,10 @@ $(document).ready(function () {
         //@TODO use loading bar instead
         $("div#loadingbar.container").show();
         //disable our button or show loading bar
-        $("#contact-submit").prop( "disabled", true);
+        $("#student-submit").prop( "disabled", true);
         //clear our form using the form id and triggering it's reset feature
-        $("#add-contact-form").trigger("reset");
+        $("#add-student-form").trigger("reset");
         $("div#loadingbar.container").fadeOut(1500);
-        
-
       }
     }
 
@@ -62,27 +65,29 @@ $(document).ready(function () {
     $.ajax(settings).done(function (response) {
       console.log(response);
       
-      $("#contact-submit").prop( "disabled", false);
-      
+      $("#student-submit").prop( "disabled", false);
       //@TODO update frontend UI 
+      document.querySelector("#add-error").textContent = null;
       $("#add-update-msg").show().fadeOut(3000);
+      $("div#loadingbar.container").fadeOut(1000);
+      
 
       //update our table 
-      getContacts();
+      getStudents();
     });
   });//end click 
 
 
   //[STEP] 6
-  //let's create a function to allow you to retrieve all the information in your contacts
+  //let's create a function to allow you to retrieve all the information in your students
   //by default we only retrieve 10 results
-  function getContacts(limit = 10, all = true) {
+  function getStudents(limit = 10, all = true) {
 
     //[STEP 7]: Create our AJAX settings
     let settings = {
       "async": true,
       "crossDomain": true,
-      "url": "https://interactivedev-8bc3.restdb.io/rest/contact",
+      "url": "https://interactivedev-8bc3.restdb.io/rest/student",
       "method": "GET", //[cher] we will use GET to retrieve info
       "headers": {
         "content-type": "application/json",
@@ -106,7 +111,7 @@ $(document).ready(function () {
         /*
         content += "<tr><td>" + response[i].name + "</td>" +
           "<td>" + response[i].email + "</td>" +
-          "<td>" + response[i].message + "</td>
+          "<td>" + response[i].mentorName + "</td>
           "<td>Del</td><td>Update</td</tr>";
         */
 
@@ -115,18 +120,20 @@ $(document).ready(function () {
         //take note that we can't use += for template literal strings
         //we use ${content} because -> content += content 
         //we want to add on previous content at the same time
-        content = `${content}<tr id='${response[i]._id}'><td>${response[i].name}</td>
+        content = `${content}<tr id='${response[i]._id}'><td>${response[i].ID}</td>
+        <td>${response[i].name}</td>
         <td>${response[i].email}</td>
-        <td>${response[i].message}</td>
-        <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-msg='${response[i].message}' data-name='${response[i].name}' data-email='${response[i].email}'>Update</a></td></tr>`;
+        <td>${response[i].class}</td>
+        <td>${response[i].mentorName}</td>
+        <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-student-container' class='update' data-id='${response[i]._id}' data-studentid='${response[i].ID}' data-name='${response[i].name}' data-email='${response[i].email}' data-class='${response[i].class}' data-mentorName='${response[i].mentorName}'>Update</a></td></tr>`;
 
       }
 
       //[STEP 9]: Update our HTML content
       //let's dump the content into our table body
-      $("#contact-list tbody").html(content);
+      $("#student-list tbody").html(content);
 
-      $("#total-contacts").html(response.length);
+      $("#total-students").html(response.length);
     });
 
 
@@ -135,58 +142,70 @@ $(document).ready(function () {
   //[STEP 10]: Create our update listener
   //here we tap onto our previous table when we click on update
   //this is a delegation feature of jquery
-  //because our content is dynamic in nature, we listen in on the main container which is "#contact-list". For each row we have a class .update to help us
-  $("#contact-list").on("click", ".update", function (e) {
+  //because our content is dynamic in nature, we listen in on the main container which is "#student-list". For each row we have a class .update to help us
+  $("#student-list").on("click", ".update", function (e) {
     e.preventDefault();
     //update our update form values
-    let contactName = $(this).data("name");
-    let contactEmail = $(this).data("email");
-    let contactMsg = $(this).data("msg");
-    let contactId = $(this).data("id");
-    console.log($(this).data("msg"));
+    let studentId = $(this).data("studentid");
+    let studentName = $(this).data("name");
+    let studentEmail = $(this).data("email");
+    let studentClass = $(this).data("class");
+    let mentorName = $(this).data("mentorname");
+    let sysId = $(this).data("id");
 
-    //[STEP 11]: Load in our data from the selected row and add it to our update contact form 
-    $("#update-contact-name").val(contactName);
-    $("#update-contact-email").val(contactEmail);
-    $("#update-contact-msg").val(contactMsg);
-    $("#update-contact-id").val(contactId);
-    $("#update-contact-container").show();
+    //[STEP 11]: Load in our data from the selected row and add it to our update student form 
+    
+    $("#update-student-id").val(studentId);
+    $("#update-student-name").val(studentName);
+    $("#update-student-email").val(studentEmail);
+    $("#update-student-class").val(studentClass);
+    $("#update-mentorName").val(mentorName);
+    $("#update-sys-id").val(sysId);
 
-  });//end contact-list listener for update function
+    $("#update-student-container").show();
 
-  //[STEP 12]: Here we load in our contact form data
+  });//end student-list listener for update function
+
+  //[STEP 12]: Here we load in our student form data
   //Update form listener
-  $("#update-contact-submit").on("click", function (e) {
+  $("#update-student-submit").on("click", function (e) {
     e.preventDefault();
     //retrieve all my update form values
-    let contactName = $("#update-contact-name").val();
-    let contactEmail = $("#update-contact-email").val();
-    let contactMsg = $("#update-contact-msg").val();
-    let contactId = $("#update-contact-id").val();
-
-    console.log($("#update-contact-msg").val());
-    console.log(contactMsg);
+    let studentName = $("#update-student-name").val();
+    let studentEmail = $("#update-student-email").val();
+    let mentorName = $("#update-mentorName").val();
+    let sysId = $("#update-sys-id").val();
+    let studentId = $("#update-student-id").val();
+    let studentClass = $("#update-student-class").val();
 
     //[STEP 12a]: We call our update form function which makes an AJAX call to our RESTDB to update the selected information
-    updateForm(contactId, contactName, contactEmail, contactMsg);
-  });//end updatecontactform listener
+    updateForm(sysId,  studentId, studentName, studentEmail, studentClass, mentorName);
+  });//end updatestudentform listener
 
   //[STEP 13]: function that makes an AJAX call and process it 
   //UPDATE Based on the ID chosen
-  function updateForm(id, contactName, contactEmail, contactMsg) {
+  function updateForm(sysId, studentId, studentName, studentEmail, studentClass, mentorName) {
     //@TODO create validation methods for id etc. 
-     
-    tocheck("update-contact-name", "update-error");
-    tocheck("update-contact-email", "update-error");
-    tocheck("update-contact-msg", "update-error");
+    
+    tocheck("update-student-id", "update-error");
+    tocheck("update-student-name", "update-error");
+    tocheck("update-student-email", "update-error");
+    tocheck("update-student-class", "update-error");
+    tocheck("update-mentorName", "update-error");
     
     
     
-    var jsondata = { "name": contactName, "email": contactEmail, "message": contactMsg };
+    var jsondata = {
+      "ID": studentId,
+      "name": studentName,
+      "email": studentEmail,
+      "class": studentClass,
+      "mentorName": mentorName
+    };
     var settings = {
       "async": true,
       "crossDomain": true,
-      "url": `https://interactivedev-8bc3.restdb.io/rest/contact/${id}`,//update based on the ID
+      "url": `https://interactivedev-8bc3.restdb.io/rest/student/${sysId}`,//update based on the ID
       "method": "PUT",
       "headers": {
         "content-type": "application/json",
@@ -196,33 +215,37 @@ $(document).ready(function () {
       "processData": false,
       "data": JSON.stringify(jsondata),
       "beforeSend": function(){
-        $("#update-contact-submit").prop( "disabled", true);
+        $("#update-student-submit").prop( "disabled", true);
       }
     }
 
-    //[STEP 13a]: send our AJAX request and hide the update contact form
+    //[STEP 13a]: send our AJAX request and hide the update student form
     $.ajax(settings).done(function (response) {
       console.log(response);
-      $("#update-contact-container").fadeOut(1000);
-      $("#update-contact-submit").prop( "disabled", false)
-      //update our contacts table
-      getContacts();
+      
+      document.querySelector("#update-error").textContent = null;
+      
+      $("#update-student-submit").prop( "disabled", false);
+      $("#update-student-container").fadeOut(1000);
+      //update our students table
+      getStudents();
+      
     });
   }//end updateform function
 
 
-  $("#contact-list").on("click", ".delete", function () {
+  $("#student-list").on("click", ".delete", function () {
     let idtobedeleted = $(this).data("id");
     deleteForm(idtobedeleted);
   })
     
 
-  function deleteForm(id) {
+  function deleteForm(sysId) {
     
     var settings = {
       "async": true,
       "crossDomain": true,
-      "url": `https://interactivedev-8bc3.restdb.io/rest/contact/${id}`,
+      "url": `https://interactivedev-8bc3.restdb.io/rest/student/${sysId}`,
       "method": "DELETE",
       "headers": {
         "content-type": "application/json",
@@ -237,7 +260,7 @@ $(document).ready(function () {
     $.ajax(settings).done(function (response) {
       console.log(response);
       $("div#loadingbar.container").fadeOut(1500); 
-      getContacts();
+      getStudents();
       
     }); 
   }
@@ -248,7 +271,7 @@ $(document).ready(function () {
 function tocheck(idcheck, area) {
   const inpObj = document.getElementById(idcheck);
   if (!inpObj.checkValidity()) {
-    document.getElementById(area).innerHTML = inpObj.validationMessage;
+    document.getElementById(area).innerHTML = `${inpObj.validationMessage} Located in (${idcheck})`;
     throw new Error();
   } 
 } 
